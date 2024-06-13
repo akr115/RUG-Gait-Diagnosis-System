a26346cc38f6e1588b8dbf0bd9adde5db845db21
nikolazxvkovic <nikolazxvkovic@gmail.com>
2024-06-13 14:03:38 +0200

    upload files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import umcg_logo from './umcg-logo.png';
-import rug_logo from './rug-logo.png'
+import rug_logo from './rug-logo.png';
 import Auth from './Auth';
 
 function Home() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (!selectedFile) {
+      alert("Please select a file first!");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    fetch("/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("File uploaded successfully:", data);
+      })
+      .catch(error => {
+        console.error("Error uploading file:", error);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={umcg_logo} className='umcg-logo'/>
+        <img src={umcg_logo} className='umcg-logo' alt="UMCG Logo" />
         <h1>Gait Diagnosis System UMCG</h1>
-        <img src={rug_logo} className='umcg-logo'/>
-
+        <img src={rug_logo} className='umcg-logo' alt="RUG Logo" />
       </header>
       <div className="data-box">
-        <button>Upload Data</button>
+        <input type="file" accept=".c3d,.xlsx" onChange={handleFileChange} />
+        <button onClick={handleUpload}>Upload Data</button>
       </div>
     </div>
   );
--- front-end/src/Auth.js
+++ front-end/src/Auth.js
@@ -1,13 +1,17 @@
 import React from "react";
+import umcg_logo from './umcg-logo.png';
+import rug_logo from './rug-logo.png'
 import './Auth.css';
 
 function Auth() {
     return (
         <div className="Auth">
+            <div className="header">
+                <img src="{ umcg-logo }" alt="UMCG Logo" className="logo left"/>
+                <h2>Gait Diagnosis System UMCG</h2>
+                <img src="{ rug-logo }" alt="University of Groningen Logo" className="logo right"/>
+            </div>
             <div className="form-box">
-                <div className="header">
-                    <h2>Login to Gait Diagnosis System</h2>
-                </div>
                 <form className="auth-box">
                     <div className="input">
                         <p>Username</p>
